3b8badd45a3bcd8db63ecfae4c6c54ba3b4e827c
Martnick1 <Martnickmatthew@gmail.com>
2018-04-24 03:06:06 -0500

    last update for the night

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,93 @@
- var targetNumber = 0;
-
-  $("#number-to-guess").text(targetNumber);
-
-  var counter = 100;
+var targetNumber = 0;
 
 
-  var numberOptions = [10, 13, 22, 17];
-  var increment = numberOptions[Math.round(Math.random())];
+$("#number-to-guess").text(targetNumber);
 
-  $("#goodguy").on("click", ".kain", function() {
-    counter -= increment;
-  }
-    $("#goodguy").on("click", ".cloud", function() {
-      counter -= increment;
-    }
+let enemyHp = 250;
+
+var kainWeapon = 7;
+var kainStrength = 1 + Math.floor(Math.random() * 10);
+var kainAtk = kainWeapon + kainStrength;
+
+$("#kain").on("click", ".kain", function () {
+	console.log("one");
+
+	let newScore = enemyHp - kainAtk;
 
 
 
-    alert("New score: " + counter);
 
-    if (counter === targetNumber) {
-      alert("You win!");
-    }
+	var cloudWeapon = 5;
+	var cloudStrength = 1 + Math.floor(Math.random() * 10);
+	var cloudAtk = cloudWeapon + cloudStrength;
 
-    else if (counter <= targetNumber) {
-      alert("You lose!!");
-    }
+	$("#cloud").on("click", ".cloud", function () {
+		console.log("one");
 
-  });
+		let newScore = enemyHp - cloudAtk;
+
+		alert("New score: " + newScore);
+
+
+		var gilgWeapon = 7;
+		var gilgStrength = 1 + Math.floor(Math.random() * 12);
+		var gilgAtk = gilgWeapon + gilgStrength;
+
+		$("#gilg").on("click", ".gilg", function () {
+			console.log("one");
+
+			let newScore = enemyHp - gilgAtk;
+
+			alert("New score: " + newScore);
+
+
+
+			var golbezWeapon = 7;
+			var golbezStrength = 1;
+			var golbezAtk = golbezWeapon + golbezStrength;
+	
+			$("#golbez").on("click", ".golbez", function () {
+				console.log("one");
+	
+				let newScore = enemyHp - golbezAtk;
+	
+				alert("New score: " + newScore);
+
+			if (newScore <= 0) {
+				alert("You win!");
+			} else if (counter >= targetNumber) {
+				alert("You lose!!");
+			}
+
+			document.getElementById("counter").innerHTML = newScore;
+
+
+
+		})
+		})
+	})
+
+});
+
 
 
 // Everything below is previous code I had tried to get to work. After failing to get it to work 
 // I tried to rework the crystal game sample as a starting point for the combat in the RPG however I keep getting stuck.
 // images won't load if I have them in the JS file instead of just the HTML and right now I can't even get the .onclick
 // to work right for just one image. Since time was starting to run short I started working on the CSS to at least
-// make it look good if it's not going to work right   
+// make it look good if it's not going to work right  
 
-  
-	// atk setup
+/* Edit: This is as functional as I can get the game to run. I added the additional characters in and when
+things started to actually work I attempted to put together an array for the characters and their stats but I was unable to 
+get that to run appropriately. I think in that case I'd have to designate a seperate class to call the character variables from 
+that array which I'll have to try more of later. To test this just click on the characters like you would for
+the crystal game. Also the Hit Point counter isn't subtracting right but I'll figure that out. 
+
+
+
+
+
+// atk setup
 /*
 
 	var enemyHP = 0;
@@ -84,9 +134,8 @@ if(atkButton && atkButton.style) {
 }
 
 
-
-
-/*	var enemyHP = 0;
+----------------------------------------------------------------------------------
+	var enemyHP = 0;
 
 	$("#number-to-guess").text(counter);
 	
@@ -116,10 +165,10 @@ if(atkButton && atkButton.style) {
 	  if (counter <= enemyHP) {
 		alert("You win!");
 	  }
-	});*/
+	});
 	
-/*
 
+---------------------------------------------------------------------
 
 	//Object declaration for characters
 	var character = [
@@ -149,8 +198,7 @@ if(atkButton && atkButton.style) {
 	$("#attack").empty();
 	$('#attackButton').empty();
 
-
-	function playGame(){
+---------------------------------------------------------------
 		
 	}
 	//basic attack set up
