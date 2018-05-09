848529366e4375473de6a4eba393d83dd34f78a5
Martnick1 <Martnickmatthew@gmail.com>
2018-05-08 22:21:51 -0500

    not a substantial update. Got character selection screen to populate char choices however can't seem t o chose one to move to the party array. Updated test character object to include traits needed for later.

--- actual_concept/assets/javascript/game.js
+++ actual_concept/assets/javascript/game.js
@@ -3,12 +3,25 @@ $(document).ready(function () {
 
 
 	var characters = {
+
+		// Kain is the model that all character objects will be based on
+		// once his stats and moveset are functioning the rest will adapt 
+		// the same set up with variations 
 		"Kain Highwind": {
 			name: "Kain Highwind",
 			health: 120,
-			attack: 8,
+			strength: 8,
 			imageUrl: "../images/kainSml.png",
-			enemyAttackBack: 15
+			enemyAttackBack: 15,
+			moveSet: {
+				attack: 8,
+				jump: 16,
+				skill: {
+					lancet: 20,
+					tornado: 27
+				},
+				defend: 0,
+			}
 		},
 
 		"Cloud Strife": {
@@ -31,7 +44,7 @@ $(document).ready(function () {
 			name: "Golbez",
 			health: 120,
 			attack: 8,
-			imageUrl: "../images/golbezSml.png",
+			imageUrl: "../assets/images/golbezSml.png",
 			enemyAttackBack: 15
 		},
 
@@ -64,26 +77,49 @@ $(document).ready(function () {
 
 
 	// designates which frame is shown on load
-	$("#game-start-screen").hide(); 
-	$("#team-character-select").hide(); 
-	$("#combat-arena").show();
+	$("#game-start-screen").show();
+	$("#team-character-select").hide();
+	$("#combat-arena").hide();
 
 	//id="team-character-select"
 
-	// renders char to the page
+
 	var renderCharacter = function (character, renderArea) {
-		// this block of code builds char card and renders it to the page
-		// needs to happen on both select screen and combat arena
-
-		var charDiv = $("<div id='attacker-one' data-name='" + character.name + "'>")
-		var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+		// this block of code builds the character card, and renders it to the page.
+		var charDiv = $("<div class='character' data-name='" + character.name + "'>");
 		var charName = $("<div class='character-name'>").text(character.name);
+		var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
 		var charHealth = $("<div class='character-health'>").text(character.health);
-
-
 		charDiv.append(charName).append(charImage).append(charHealth);
 		$(renderArea).append(charDiv);
-	}
+
+	};
+
+	var initializeGame = function () {
+		$("#start-game").click(function () {
+			// Loop through the characters object and call the renderCharacter function on each character to render their card
+			for (var key in characters) {
+				renderCharacter(characters[key], "#team-character-select");
+			}
+		}, )
+	};
+
+
+	initializeGame();
+
+	$(".character").click(function () {
+		$(this).toggleClass("off");
+	});
+
+	$(".character").click(function partyDisplay() {
+		// Loop through the characters object and call the renderCharacter function on each character to render their card
+		party.push(".character");
+		document.getElementById("selected-characters").innerHTML = party;
+		console.log(party);
+	});
+	console.log(party);
+
+/*
 
 	// function to load all characters into the select screen
 	var initializeGame = function () {
@@ -96,7 +132,7 @@ $(document).ready(function () {
 	// runs initializeGame function
 	initializeGame();
 
-	// function handles updating the selected players
+	// function handles updating the selected characters
 	// function will also place the character based on the areaRender chosen (#player-section)
 
 	var updateCharacters = function (charObj, areaRender) {
@@ -105,7 +141,7 @@ $(document).ready(function () {
 		renderCharacter(charObj, areaRender);
 
 	};
-
+*/
 	//=====================================================
 	// following commented out function will be replaced with enemy array seperate from player characters, keeping it here now for reference
 	/*
@@ -114,7 +150,7 @@ $(document).ready(function () {
 		for (var i = 0; i < enemyArr.length; i++) {
 			renderCharacter(enemyArr[i], "#available-to-attack-section");
 		}
-	};*/
+	};
 	//=====================================================
 
 
@@ -155,27 +191,80 @@ $("#team-character-select").on("click", ".character", function () {
 	var name = $(this).attr("data-name");
 
 	//if a player character has not yet been chosen 
-	if (!attacker1) {
+	if (!attacker) {
 		// we populate attacker with the selected character and push to the selected characters and party array
-		attacker1 = characters[name];
+		attackerChoice = characters[name];
 	};
 
-	if (!attacker2) {
+/*	if (!attacker2) {
 		attacker2 = characters[name];	
 	}
+*/
 
-	// we then loop through the remaining characters and push them to the unUsed array
-	for (var key in characters) {
-		if (key !==name) {
-			unUsed.push(characters[key]);
+	/*
+		// we then loop through the remaining characters and push them to the unUsed array
+		for (var key in characters) {
+			if (key !==name) {
+				unUsed.push(characters[key]);
+			}
 		}
-	}
+
+
+
+		// then render our selected characters to #player-section
+		updateCharacter(attacker1, "#attacker-one");
+		updateCharacter(attacker2, "#attacker-two");
+
+		 
+	});
+
+	// here I need to create and populate the moveset
+	*/
+
+	//-------API----------------------------------------------------------------
+
+	// World Clock API will be used to create a day/night cycle. May change over to one that can adapt local time if that's possible. 
+	// Will see what can be done in conjunction with googlemaps
+	var timeURL = "http://worldclockapi.com/api/json/utc/now";
+
+	$.ajax({
+		url: timeURL,
+		method: "GET"
+	}).then(function (response) {
+		$("#currentDateTime").text(JSON.stringify(response));
+		console.log(response);
+	});
+
+
+	//-----Frame Transitions for content area-----------------------------------
+
+	// Homepage begins on start screen with following screens hidden.
+	// this section is designated for those hidden sections and creates initial 
+	// function for section progression.
+
+
+
+
+	$("#game-start-screen").click(function () {
+		$(this).toggleClass("off");
+	});
+	var startToggle;
+	$("#start-game").click(function () {
+		if (startToggle) {
+			startToggle.appendTo("body");
+			startToggle = null;
+		} else {
+			startToggle = $("#game-start-screen").detach();
+			$("#team-character-select").show("slow");
+		}
+	});
 
 	// Once characters are selected .click function proceeds to combat screen and detachs character select screen
 	$("#team-character-select").click(function () {
 		$(this).toggleClass("off");
 	});
-	var selectToggle; $("#char-select").click(function () {
+	var selectToggle;
+	$("#char-select").click(function () {
 		if (selectToggle) {
 			selectToggle.appendTo("body");
 			selectToggle = null;
@@ -185,68 +274,22 @@ $("#team-character-select").on("click", ".character", function () {
 		}
 	});
 
-	// then render our selected characters to #player-section
-	updateCharacter(attacker1, "#attacker-one");
-	updateCharacter(attacker2, "#attacker-two");
-
-	 
-});
-
-
-//-------API----------------------------------------------------------------
-
-// World Clock API will be used to create a day/night cycle. May change over to one that can adapt local time if that's possible. 
-// Will see what can be done in conjunction with googlemaps
-var timeURL = "http://worldclockapi.com/api/json/utc/now";
-
-$.ajax({
-	url: timeURL,
-	method: "GET"
-}).then(function (response) {
-	$("#currentDateTime").text(JSON.stringify(response));
-	console.log(response);
-});
-
-
-//-----Frame Transitions for content area-----------------------------------
-
-// Homepage begins on start screen with following screens hidden.
-// this section is designated for those hidden sections and creates initial 
-// function for section progression.
+	$("#combat-arena").click(function () {
+		$(this).toggleClass("off");
+	});
+	var combatToggle;
+	$("#end-match").click(function () {
+		if (combatToggle) {
+			combatToggle.appendTo("body");
+			combatToggle = null;
+		} else {
+			combatToggle = $("#combat-arena").detach();
+			$("#game-start-screen").show("slow");
+		}
+	});
 
 
 
-
-$("#game-start-screen").click(function () {
-	$(this).toggleClass("off");
-});
-var startToggle; $("#start-game").click(function () {
-	if (startToggle) {
-		startToggle.appendTo("body");
-		startToggle = null;
-	} else {
-		startToggle = $("#game-start-screen").detach();
-		$("#team-character-select").show("slow");
-	}
-});
-
-
-
-$("#combat-arena").click(function () {
-	$(this).toggleClass("off");
-});
-var combatToggle; $("#end-match").click(function () {
-	if (combatToggle) {
-		combatToggle.appendTo("body");
-		combatToggle = null;
-	} else {
-		combatToggle = $("#combat-arena").detach();
-		$("#game-start-screen").show();
-	}
-});
-
-
-
-//----------End Frame Transitions-----------------------------------------
+	//----------End Frame Transitions-----------------------------------------
 
 });
