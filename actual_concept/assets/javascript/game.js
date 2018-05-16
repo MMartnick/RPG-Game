d8ecfb35cfe747f57e3fce2b35e5460292c70016
Martnick1 <Martnickmatthew@gmail.com>
2018-05-16 00:44:14 -0500

    it's better

--- actual_concept/assets/javascript/game.js
+++ actual_concept/assets/javascript/game.js
@@ -13,24 +13,68 @@ function difficulty() {
 
 
 
-
+var locate;
+var lat;
+var lon;
 var x = document.getElementById("demo");
 
 function getLocation() {
+	
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition);
-		lat = Position.coords.latitude;
-		lon = Position.coords.longitude;
+		lat = position.coords.latitude;
+		lon = position.coords.longitude;
+		locate = lat + lon;
 
 	} else {
 		x.innerHTML = "Geolocation is not supported by this browser.";
 	}
-}
+
+console.log(locate);
+	return locate;
+	
+};
 
 function showPosition(position) {
-	x.innerHTML = "Latitude: " + position.coords.latitude +
-		"<br>Longitude: " + position.coords.longitude;
-}
+	x.innerHTML = "Latitude: " + Math.ceil(position.coords.latitude) +
+		"<br>Longitude: " + Math.ceil(position.coords.longitude);
+};
+
+//-------API----------------------------------------------------------------
+
+// World Clock API will be used to create a day/night cycle. May change over to one that can adapt local time if that's possible. 
+
+
+var timeURL = "https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lon + "&timestamp=1458000000&key=AIzaSyBrACSj3zHNkqz4JO7ypicGFCE-We1aco8";
+$.ajax({
+	url: timeURL,
+	method: "GET"
+}).then(function (response) {
+	console.log(response);
+
+	//$("#currentDateTime").innerHTML(response);
+});
+
+/*
+
+var locationURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBrACSj3zHNkqz4JO7ypicGFCE-We1aco8";
+
+$.ajax({
+	url: locationURL,
+	method: "GET"
+}).then(function (geolocate) {
+	console.log(response);
+
+	var lon = response.location.lng ;
+	var lat = response.location.lat;
+	$("#currentDateTime").innerHTML(response);
+
+});*/
+
+
+
+
+//-----------------------------------------------------------------------
 
 //-----weather API
 
@@ -38,7 +82,7 @@ var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
 // Here we are building the URL we need to query the database
 var weatherURL = "https://api.openweathermap.org/data/2.5/weather?" +
-	"lat=" + latitude + "&" + longitude + "=101&appid=" + APIKey;
+	"lat=" + lat + "&" + lon + "=101&appid=" + APIKey;
 
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
@@ -69,9 +113,10 @@ $.ajax({
 
 
 
-	
-$(document).ready(function () {
 
+
+
+$(document).ready(function () {
 
 
 
@@ -158,7 +203,8 @@ $(document).ready(function () {
 			enemyAttackBack: 15
 		},
 
-		"Ultimecia": {
+
+		"spiderhouse": {
 			name: "Ultimecia",
 			health: 120,
 			attack: 8,
@@ -166,14 +212,12 @@ $(document).ready(function () {
 			enemyAttackBack: 15
 		},
 
-	};
 
-	var spiderHouse = {
-		"Ultimecia": {
-			name: "Ultimecia",
+		"homeTest": {
+			name: "Golbee",
 			health: 120,
 			attack: 8,
-			imageUrl: "assets/images/ultimecia.png",
+			imageUrl: "assets/images/golbezSml.png",
 			enemyAttackBack: 15
 		},
 
@@ -190,11 +234,10 @@ $(document).ready(function () {
 	// will be populated when player selects a character team
 
 	var dude;
-	var badDude;
+	//var badDude;
 	var charCounter = 2;
 	var killCount = 0;
-
-	var localEnemy;
+	var enemySelected;
 
 
 	//var combatTimer = setInterval(timer, speed);
@@ -241,7 +284,8 @@ $(document).ready(function () {
 		$(renderArea).append(charDiv);
 	};
 
-	// renders all characters to the DOM
+
+	// renders all enemies to the DOM
 	var renderEnemy = function (character, renderArea) {
 		// this block of code builds the character card, and renders it to the page.
 		var charDiv = $("<div class='character' data-name='" + character.name + "'>");
@@ -250,19 +294,10 @@ $(document).ready(function () {
 		// var charHealth = $("<div class='character-health'>").text(character.health);
 		charDiv /*.append(charName)*/ .append(charImage); //.append(charHealth);
 		$(renderArea).append(charDiv);
-
-
-
 	};
 
 
 
-	var getEnemy = function (latitude, longitude) {
-		if ((latitude = 30.295502) && (longitude = -97.7417479)) {
-			$(localEnemy = spiderHouse);
-		}
-
-	}
 
 	// renders character moveSet to the DOM
 	var renderMoveSet = function (character, renderArea) {
@@ -271,98 +306,14 @@ $(document).ready(function () {
 		attack1.addClass("combat-attack");
 		attack1.attr("data-type", character);
 		attack1.text(character);
-		var something = attack1.attr("data-type");
-		console.log(something);
+		// variable for testing to see that the character data renders a distinct button
+		var attackData = attack1.attr("data-type");
+		console.log(attackData);
+
 		$(renderArea).append(attack1);
-			//combat set up. 
-
-	// when you click the attack button, run the following game logic
-	$(".combat-attack").on("click", function () {
-		// if there is a defender, combat will occur
-		//if ($("#defender").children().length !== 0) {
-		// creates messages for our attack and our opponents counter attack
-		var whateverANJALIwants = $(this).attr("data-type"); 
-		console.log(whateverANJALIwants, "Yo");
-		var attackMessage = characters[whateverANJALIwants].name + enemyCharacters.Kefka.name + "for" + characters[whateverANJALIwants].moveSet.attack + " damage.";
-		var counterAttackMessage = enemyCharacters.Kefka.name + " did " + enemyCharacters.Kefka.attack + " damage.";
-		clearMessage();
-		
-		// Reduce defender's health by your attack value.
-
-
-
-		// If the enemey st has health
-		if (enemyCharacters.Kefka.health > 0) {
-			// Render the enemy's updated character card.
-			// updateCharacter(defender, "#defender");
-			enemyCharacters.Kefka.health -= characters[whateverANJALIwants].moveSet.attack;
-
-			console.log("kefka");
-			console.log(enemyCharacters.Kefka.health);
-			// Render the combat messages.
-			renderMessage(attackMessage);
-			renderMessage(counterAttackMessage);
-
-			// Reduce your attack health by the opponents attack value.
-			characters[whateverANJALIwants].health -= enemyCharacters.Kefka.attack;
-			console.log(characters[whateverANJALIwants]);
-			console.log(characters[whateverANJALIwants].health);
-			//render the player's updated character card.
-			//updateCharacter(attacker, "#selected-character");
-
-
-			if (killCount < enemyCharacters.length && enemyCharacters.Kefka.health <= 0) {
-				$(enemyCharacters.Kefka).detach();
-				$(enemyCharacters.Kafka).show("slow");
-				battle1.pause();
-				boss1.play();
-			}
-
-
-			// If you have less than zero health the game ends. 
-			// We call the restartGame function to allow the user to restart the game and play again.
-			if (characters[whateverANJALIwants].health <= 0) {
-				clearMessage();
-				// restartGame("Game Over");
-				window.alert("You ded")
-				$(".combat-attack").off("click");
-			}
-		} else {
-			// if the enemy has less than zero health they are defeated
-			// remove your opponents character card. 
-			// $("#defender").empty();
-
-			var gameStateMessage = "You have defeated " + enemyCharacters.Kefka.name;
-			renderMessage(gameStateMessage);
-
-			// Increment your kill count
-			killCount++;
-
-			// If you have killed all of your opponents you win
-			// Call the restartGame function to allow the user to restart the game and play again
-			if (killCount >= enemyCharacters.length) {
-				clearMessage();
-				$(".combat-attack").off("click");
-				//restartGame("You Won");
-				window.alert("You won!!!!");
-
-			}
-		}
-		// Increment turn counter. This is used for determining how much damage the player does.
-		// turnCounter++;
-		//} else {
-		// If there is no defender, render an error message
-		//clearMessage();
-		//renderMessage("No enemy here");
-
-		//}
-	});
-
+		//combat set up. 
 
 	};
-
-
-
 	// renders character info to the DOM
 	var renderInfo = function (character, renderArea) {
 		var charInfoMenu = $("<div class='char-info-menu' data-name='" + character.name + "'>");
@@ -383,34 +334,33 @@ $(document).ready(function () {
 
 
 
-	// initiallizes the game
-	var initializeGame = function () {
-		$("#start-game").click(function () {
-			// Loop through the characters object and call the renderCharacter function on each character to render their card
-			for (var key in characters) {
-
-				//renderInfo(characters[key], ".character-menu");
-				renderCharacter(characters[key], "#team-character-select");
-				//intro.play();
-				console.log(key);
-			};
 
 
-			//var result = Object.keys(enemyCharacters).map(function (key) {
-			//return [Number(key), enemyCharacters[key]];
-			for (var key in enemyCharacters) {
-				renderEnemy(enemyCharacters[key], "#enemy-section");
-				renderInfo(enemyCharacters[key], ".enemy-menu");
-				
-			};
 
+	var currentEnemy;
 
-				
-				console.log(key);
-	
-		}, )
+	var getEnemy = function (latitude, longitude) {
+
+		if ((latitude == 60.295502) && (longitude == -97.7417479)) {
+			renderEnemy(enemyCharacters.spiderhouse, "#enemy-section");
+			renderInfo(enemyCharacters.spiderhouse, ".enemy-menu");
+			currentEnemy = enemyCharacters.spiderhouse;
+
+		} 
+		else if ((latitude == 31) && (longitude == -97)) {
+			renderEnemy(enemyCharacters.homeTest, "#enemy-section");
+			renderInfo(enemyCharacters.homeTest, ".enemy-menu");
+			currentEnemy = enemyCharacters.homeTest;
+
+		} 
+		else {
+			renderEnemy(enemyCharacters.Kefka, "#enemy-section");
+			renderInfo(enemyCharacters.Kefka, ".enemy-menu");
+			currentEnemy = enemyCharacters.Kefka;
+		}
+
+		return currentEnemy;
 	};
-
 
 
 	// function for selecting characters
@@ -451,49 +401,91 @@ $(document).ready(function () {
 
 
 
-	// functions called
 
-	
-
-
-	initializeGame();
+	// when you click the attack button, run the following game logic
+	$(".combat-attack").on("click", function () {
 
 
+		var playerName = $(this).attr("data-type");
+		console.log(playerName, "Yo");
+		// creates messages for our attack and our opponents counter attack
+		var attackMessage = characters[playerName].name + enemySelected.name + "for" + characters[playerName].moveSet.attack + " damage.";
+		var counterAttackMessage = enemySelected.name + " did " + enemySelected.attack + " damage.";
+		clearMessage();
+
+		// Reduce defender's health by your attack value.
 
 
-	//-------API----------------------------------------------------------------
 
-	// World Clock API will be used to create a day/night cycle. May change over to one that can adapt local time if that's possible. 
+		// If the enemey st has health
+		if (enemySelected.health > 0) {
+			// Render the enemy's updated character card.
+			// updateCharacter(defender, "#defender");
+			enemySelected.health -= characters[playerName].moveSet.attack;
+
+			console.log(enemySelected.health);
+			// Render the combat messages.
+			renderMessage(attackMessage);
+			renderMessage(counterAttackMessage);
+
+			// Reduce your attack health by the opponents attack value.
+			characters[playerName].health -= enemySelected.attack;
+			console.log(characters[playerName]);
+			console.log(characters[playerName].health);
+			//render the player's updated character card.
+			//updateCharacter(attacker, "#selected-character");
 
 
-	var timeURL = "https://maps.googleapis.com/maps/api/timezone/json?location=" + latitude + "," + longitude + "&timestamp=1458000000&key=AIzaSyBrACSj3zHNkqz4JO7ypicGFCE-We1aco8";
-	$.ajax({
-		url: timeURL,
-		method: "GET"
-	}).then(function (response) {
-		console.log(response);
+			if (killCount < enemyCharacters.length && enemySelected.health <= 0) {
+				$(enemySelected).detach();
+				$(enemySelected).show("slow");
+				battle1.pause();
+				boss1.play();
+			}
 
-		//$("#currentDateTime").innerHTML(response);
+
+			// If you have less than zero health the game ends. 
+			// We call the restartGame function to allow the user to restart the game and play again.
+			if (characters[playerName].health <= 0) {
+				clearMessage();
+				// restartGame("Game Over");
+				//window.alert("You ded")
+				$(".combat-attack").off("click");
+			}
+		} else {
+			// if the enemy has less than zero health they are defeated
+			// remove your opponents character card. 
+			// $("#defender").empty();
+
+			var gameStateMessage = "You have defeated " + enemySelected.name;
+			renderMessage(gameStateMessage);
+
+			// Increment your kill count
+			killCount++;
+
+			// If you have killed all of your opponents you win
+			// Call the restartGame function to allow the user to restart the game and play again
+			if (killCount >= enemyCharacters.length) {
+				clearMessage();
+				$(".combat-attack").off("click");
+				//restartGame("You Won");
+				window.alert("You won!!!!");
+
+			}
+		}
+		// Increment turn counter. This is used for determining how much damage the player does.
+		// turnCounter++;
+		//} else {
+		// If there is no defender, render an error message
+		//clearMessage();
+		//renderMessage("No enemy here");
+
+		//}
 	});
 
 
-	var locationURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBrACSj3zHNkqz4JO7ypicGFCE-We1aco8";
-
-	$.ajax({
-		url: locationURL,
-		method: "GET"
-	}).then(function (geolocate) {
-		console.log(response);
-
-		var longitude = response.location.lng;
-		var latitude = response.location.lat;
-		$("#currentDateTime").innerHTML(response);
-
-	});
 
 
-
-	
 
 
 	//-----Frame Transitions for content area-----------------------------------
@@ -504,6 +496,7 @@ $(document).ready(function () {
 
 	var startToggle;
 	$("#start-game").click(function () {
+
 		if (startToggle) {
 			startToggle.appendTo("body");
 			startToggle = null;
@@ -517,6 +510,8 @@ $(document).ready(function () {
 
 	var selectToggle;
 	$("#char-select").click(function () {
+		enemySelected = getEnemy(lat, lon);
+
 		if (selectToggle) {
 			selectToggle.appendTo("body");
 			selectToggle = null;
@@ -546,4 +541,22 @@ $(document).ready(function () {
 
 	//----------End Frame Transitions-----------------------------------------
 
+
+	// initiallizes the game
+	var initializeGame = function () {
+		$("#start-game").click(function () {
+			// Loop through the characters object and call the renderCharacter function on each character to render their card
+			for (var key in characters) {
+				//renderInfo(characters[key], ".character-menu");
+				renderCharacter(characters[key], "#team-character-select");
+				//intro.play();
+				console.log(key);
+			};
+
+
+		});
+	};
+
+
+	initializeGame();
 });
